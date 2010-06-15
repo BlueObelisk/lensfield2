8b2a4081cb2976c436307df33c9a7dcf2d33cd36
Sam Adams <seadams@gmail.com>
2010-06-15 22:22:34 +0100

    Added ConfigurationException

---
+++ lensfield2-core/src/main/java/org/lensfield/ConfigurationException.java
@@ -0,0 +1,23 @@
+package org.lensfield;
+
+/**
+ * @author sea36
+ */
+public class ConfigurationException extends LensfieldException {
+
+    public ConfigurationException() {
+    }
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConfigurationException(Throwable cause) {
+        super(cause);
+    }
+
+}
